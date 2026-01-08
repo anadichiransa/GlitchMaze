0af5de9931d6a38e995a9d7bf774448eced5daa0
Anadi Chiransa <anadipiyamini@gmail.com>
2026-01-08 15:45:44 +0530

    Added basic window settings

--- Main.java
+++ Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args){
 
         JFrame window = new JFrame();
-        
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("The Glitch Maze");
+
 
     }
 }
