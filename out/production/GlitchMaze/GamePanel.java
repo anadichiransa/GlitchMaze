487b618f7ee9ddde07eb82123249865971ca06c3
Anadi Chiransa <anadipiyamini@gmail.com>
2026-01-09 20:44:14 +0530

    Updated generate map

--- out/production/GlitchMaze/GamePanel.java
+++ out/production/GlitchMaze/GamePanel.java
@@ -1,6 +1,7 @@
 import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.Color;
+import java.util.Random;
 
 public class GamePanel extends JPanel{
 
@@ -11,13 +12,23 @@ public class GamePanel extends JPanel{
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    int[][] map = new int[maxScreenCol][maxScreenRow];
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+
+        generateMap();
     }
 
+    public void generateMap(){
+        for (int x=0;x<maxScreenCol; x++){
+            for (int y =0 ; y<maxScreenRow;y++){
+                map[x][y] =0 ;
+            }
+        }
+    }
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -33,3 +44,5 @@ public class GamePanel extends JPanel{
 
 
 }
+
+
